833559489d15eaf22cf3a62e2c1a0d6cffd5f072
Estefanio N Santos <estefanions@gmail.com>
2026-05-23 19:39:51 -0300

    [#2605231857] refactor(filter): modernize SeparatorToSeparatorWordFilter for PHP 8.2
    - Refactored SeparatorToSeparatorWordFilter.
    - Applied 'readonly' to replacementSeparator property promotion.
    - Removed redundant is_string() validation check and InvalidArgumentException.
    - Standardized internal separator calls to match previous refactors.

--- Rubricate/Filter/Word/SeparatorToSeparatorWordFilter.php
+++ Rubricate/Filter/Word/SeparatorToSeparatorWordFilter.php
@@ -5,31 +5,20 @@ declare(strict_types=1);
 namespace Rubricate\Filter\Word;
 
 use Rubricate\Filter\IGetFilter;
-use InvalidArgumentException;
 
-class SeparatorToSeparatorWordFilter extends 
-    AbstractSeparatorWordFilter implements IGetFilter
+class SeparatorToSeparatorWordFilter extends AbstractSeparatorWordFilter implements IGetFilter
 {
     public function __construct(
         string $searchSeparator = ' ',
-        private string $replacementSeparator = '-'
+        private readonly string $replacementSeparator = '-'
     ) {
         parent::__construct($searchSeparator);
     }
 
     public function getFilter(string $value): string
     {
-        if (!is_string($this->replacementSeparator)) {
-            throw new InvalidArgumentException(
-                sprintf(
-                    '"%s" is not a valid replacementSeparator.',
-                    $this->replacementSeparator
-                )
-            );
-        }
-
         return str_replace(
-            parent::getSeparator(),
+            $this->getSeparator(),
             $this->replacementSeparator,
             $value
         );
