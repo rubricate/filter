a88cdce9f9ee86b74643209861f1d905594dc440
Estefanio N Santos <estefanions@gmail.com>
2026-05-23 19:33:13 -0300

    [#2605231857] refactor(filter): clean up CamelCaseToSeparatorWordFilter boilerplate
    - Refactored CamelCaseToSeparatorWordFilter for PHP 8.2.
    - Removed redundant constructor that only replicated parent initialization.
    - Simplified separator retrieval by replacing parent:: invocation with $this->.

--- Rubricate/Filter/Word/CamelCaseToSeparatorWordFilter.php
+++ Rubricate/Filter/Word/CamelCaseToSeparatorWordFilter.php
@@ -6,18 +6,13 @@ namespace Rubricate\Filter\Word;
 
 use Rubricate\Filter\IGetFilter;
 
-class CamelCaseToSeparatorWordFilter extends 
-    AbstractSeparatorWordFilter implements IGetFilter
+class CamelCaseToSeparatorWordFilter extends AbstractSeparatorWordFilter implements IGetFilter
 {
-    public function __construct(string $separator = ' ')
-    {
-        parent::__construct($separator);
-    }
-
     public function getFilter(string $value): string
     {
         $words = preg_split('/(?<=[a-z])(?=[A-Z])/x', $value) ?: [];
 
-        return implode(parent::getSeparator(), $words);
+        return implode($this->getSeparator(), $words);
     }
 }
+
