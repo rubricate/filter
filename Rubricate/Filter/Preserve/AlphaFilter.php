b442bf0144da6be539e870ce3b7cbe042c370bbe
Estefanio N Santos <estefanions@gmail.com>
2026-05-23 21:03:20 -0300

    [#2605232008] refactor: rename AlphaPreserveFilter to AlphaFilter

--- Rubricate/Filter/Preserve/AlphaFilter.php
+++ Rubricate/Filter/Preserve/AlphaFilter.php
@@ -6,7 +6,7 @@ namespace Rubricate\Filter\Preserve;
 
 use Rubricate\Filter\IGetFilter;
 
-class AlphaPreserveFilter implements IGetFilter
+class AlphaFilter implements IGetFilter
 {
     private const PATTERN = '/[^a-zA-Z]/';
 
